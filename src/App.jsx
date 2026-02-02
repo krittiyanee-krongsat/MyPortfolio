d24d880825c227b059b212590cd9e9a2268f454c
Krittiyanee Krongsat <krittiyanee.kron@gmail.com>
2026-02-02 18:09:40 +0700

    update 3D and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "./sections/Hero";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   return (
     <>
+        <Navbar />
         <Hero />
     </>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,28 @@
-export const words = [
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
     { text: 'Ideas', imgPath: '/images/ideas.svg'},
     { text: 'Concepts', imgPath: '/images/concepts.svg'},
 ]
+
+export {
+    navLinks,
+    words
+}
