8593b972c04743c2a5c5a112fca745c22d4c684e
Krittiyanee Krongsat <krittiyanee.kron@gmail.com>
2026-02-25 03:54:11 +0700

    เพิ่ม CardExperiences

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,3 +1,4 @@
+import GlowCard from "../components/GlowCard"
 import TitleHeader from "../components/TitleHeader"
 import { experiences } from "../constants"
 
@@ -12,7 +13,13 @@ const ExperienceSection = () => {
                     <div className="relative z-50 xl:space-y-32 space-y-10">
                       {experiences.map((card) => (
                         <div key={card.title} className="exp-card-wrapper">
-                          {card.title}
+                          <div className="xl:w-2/6">
+                            <GlowCard card={card}>
+                              <div>
+                                <img src={card.imgPath} alt={card.title}/>
+                              </div>
+                            </GlowCard>
+                          </div>
                         </div>
                       ))}
                     </div>
