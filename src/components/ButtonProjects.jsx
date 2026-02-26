09444631db42e7e47a23b9f53b247ded773acb51
Krittiyanee Krongsat <krittiyanee.kron@gmail.com>
2026-02-26 19:22:07 +0700

    เพิ่มรูปภาพ

--- src/components/ButtonProjects.jsx
+++ src/components/ButtonProjects.jsx
@@ -1,6 +1,6 @@
 const ButtonProjects = ({ text, className, id }) => {
   return (
-    <a href="/images/WallpaperCom.jpg" className={`${className ?? ''} cta-wrapper`} id={id}> 
+    <a href="/images/Certificate_1.jpg" className={`${className ?? ''} cta-wrapper`} id={id}> 
         <div className="cta-button-card group">
             <p className="text">{text}</p>
         </div>
