a956ef385ed580d675026d14119d33173f5853b0
Krittiyanee Krongsat <krittiyanee.kron@gmail.com>
2026-03-02 15:21:20 +0700

    ตกแต่ง 3D

--- src/components/Contact/ComputerExperience.jsx
+++ src/components/Contact/ComputerExperience.jsx
@@ -7,13 +7,37 @@ import ComputerPack from "./ComputerPack"
 
 const ComputerExperience = () => {
     return (
-        <Canvas>
-        <ambientLight/>
-        <OrbitControls />
-        <Suspense fallback={null}>
-          <ComputerPack/>
-        </Suspense>
-        <Environment preset={"sunset"}/>
+      <Canvas shadows camera={{ position: [0, 0, 15], fov: 45 }}>
+          <ambientLight intensity={0.5} color="#94A3B8"/>
+          <directionalLight position={[5, 5, 5]} intensity={2.5} color="#fff4e6" />
+          
+          <directionalLight
+            position={[5,9,1]}
+            castShadow
+            intensity={2.5}
+            color="#94A3B8"
+          />
+
+          <OrbitControls
+            enableZoom={false}
+            minPolarAngle={Math.PI / 5}
+            maxPolarAngle={Math.PI / 2}
+          />
+
+          <group scale={[1, 1, 1]}>
+            <mesh
+              receiveShadow
+              position={[0, -1.5, 0]}
+              rotation={[-Math.PI / 2, 0, 0]}
+            >
+              <planeGeometry args={[30, 30]} />
+              <meshStandardMaterial color="#94A3B8" />
+            </mesh>
+          </group>
+
+          <group scale={1} position={[0, -1.50, -2]} castShadow>
+            <ComputerPack/>
+          </group>
       </Canvas>
     )
 }
